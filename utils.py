01b972e77e44856e2b720e9ea88a84f5b529cc00
Arthur Zhang <azhangvo@gmail.com>
2023-07-09 22:42:08 -0400

    optimize convolution

--- utils.py
+++ utils.py
@@ -2,9 +2,39 @@ import cv2
 import numpy as np
 
 
-def convolve(mat, kernel):  # Assume mat is 2d grayscale and kernel is square
+# 1. reshape image into image cols
+# 2. reshape kernel into linear shape
+# 3. matrix multiply kernel and image cols
+# 4. reshape result into convolved image
+def convolve(mat, kernel):
     kernel_size = kernel.shape[0]
-    convolved_mat = np.zeros((mat.shape[0] - kernel_size + 1, mat.shape[1] - kernel_size + 1))
+
+    out_height = mat.shape[0] - kernel_size + 1
+    out_width = mat.shape[1] - kernel_size + 1
+
+    i0 = np.repeat(np.arange(kernel_size), kernel_size)
+    i1 = np.repeat(np.arange(out_height), out_width)
+
+    i = i0.reshape((-1, 1)) + i1.reshape((1, -1))
+
+    j0 = np.tile(np.arange(kernel_size), kernel_size)
+    j1 = np.tile(np.arange(out_width), out_height)
+
+    j = j0.reshape((-1, 1)) + j1.reshape((1, -1))
+
+    img_cols = mat[i, j]
+    kernel_col = kernel.reshape((1, -1))
+
+    return np.matmul(kernel_col, img_cols).reshape((out_height, out_width))
+
+
+def naive_convolve(mat, kernel):
+    kernel_size = kernel.shape[0]
+
+    out_height = mat.shape[0] - kernel_size + 1
+    out_width = mat.shape[1] - kernel_size + 1
+
+    convolved_mat = np.zeros((out_height, out_width))
 
     for i in range(convolved_mat.shape[0]):
         for j in range(convolved_mat.shape[1]):
@@ -22,9 +52,13 @@ if __name__ == '__main__':
     cv2.imshow("grayscale", grayscale)
 
     print(grayscale)
+    print(grayscale.shape)
 
     kernel = np.array([[1, 0, -1], [2, 0, -2], [1, 0, -1]])
     convolved_x = convolve(grayscale, kernel)
+    # naive_convolved_x = naive_convolve(grayscale, kernel)
+    #
+    # np.testing.assert_array_equal(convolved_x, naive_convolved_x)
 
     convolved_x[convolved_x < 0] = 0
     convolved_x[convolved_x > 255] = 255
