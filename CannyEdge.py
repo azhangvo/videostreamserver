5ec6411c9b8a2d7c04a7af91710d66cb03191ad2
Arthur Zhang <azhangvo@gmail.com>
2023-07-14 15:39:56 -0400

    optimizations with numba

--- CannyEdge.py
+++ CannyEdge.py
@@ -1,9 +1,10 @@
 import time
 import numpy as np
 import cv2
+from scipy.ndimage import maximum_filter
 
 from Server import Server, get_ip
-from utils import Canny, HoughLines
+from utils import Canny, HoughLines, CannyJIT
 
 server = Server()
 
@@ -20,32 +21,34 @@ while True:
         continue
 
     img = server.get_img()
-    canny = cv2.Canny(img, 100, 200)
-    # canny = Canny(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)).astype(np.uint8)
+    # canny_truth = cv2.Canny(img, 100, 200)
+    canny = CannyJIT(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)).astype(np.uint8)
 
     cv2.imshow("canny", canny)
+    # cv2.imshow("canny truth", canny_truth)
 
     hough = HoughLines(canny)
+    max_filter = maximum_filter(hough[0], size=7)
 
-    # cv2.imshow("hist", hough[0][0] / np.max(hough[0][0]))
+    # cv2.imshow("hist", hough[0] / np.max(hough[0]))
 
-    mat = np.repeat(hough[0][0] / max(np.max(hough[0][0]), 0.01), 3).reshape((*hough[0][0].shape, 3))
-    mat[(hough[1] == hough[0][0]) & (hough[1] >= 200), 0] = 0
-    mat[(hough[1] == hough[0][0]) & (hough[1] >= 200), 1] = 0
-    mat[(hough[1] == hough[0][0]) & (hough[1] >= 200), 2] = 255
+    mat = np.repeat(hough[0] / max(np.max(hough[0]), 0.01), 3).reshape((*hough[0].shape, 3))
+    mat[(max_filter == hough[0]) & (max_filter >= 200), 0] = 0
+    mat[(max_filter == hough[0]) & (max_filter >= 200), 1] = 0
+    mat[(max_filter == hough[0]) & (max_filter >= 200), 2] = 255
 
     mat = cv2.resize(mat, (1000, 1000))
 
     cv2.imshow("hist2", mat)
 
-    lines = np.array(np.where((hough[1] == hough[0][0]) & (hough[1] >= 200))).transpose()
+    lines = np.array(np.where((max_filter == hough[0]) & (max_filter >= 200))).transpose()
     # lines = cv2.HoughLines(canny, 1, np.pi/180, 200)
 
     if lines is not None:
         for line in lines:
             rho, theta = line
-            rho = hough[0][1][rho]
-            theta = hough[0][2][theta]
+            rho = hough[1][rho]
+            theta = hough[2][theta]
             a = np.cos(theta)
             b = np.sin(theta)
             x0 = a * rho
--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 import math
 
+from fast_histogram import histogram2d
+from numba import jit, njit, float64, int64
 import cv2
 import numpy as np
 from scipy import signal
@@ -12,28 +14,47 @@ FFT_CONVOLVE = True
 # 2. reshape kernel into linear shape
 # 3. matrix multiply kernel and image cols
 # 4. reshape result into convolved image
-def convolve(mat, kernel):
-    if FFT_CONVOLVE:
-        return signal.fftconvolve(mat, kernel)
-    kernel_size = kernel.shape[0]
+@jit(nopython=True)
+def convolve(image, kernel):
+    m, n = kernel.shape
+    if m == n:
+        y, x = image.shape
+        y = y - m + 1
+        x = x - m + 1
+        new_image = np.zeros((y, x))
+        for i in range(y):
+            for j in range(x):
+                new_image[i][j] = np.sum(image[i:i + m, j:j + m] * kernel)
+    return new_image
 
-    out_height = mat.shape[0] - kernel_size + 1
-    out_width = mat.shape[1] - kernel_size + 1
 
-    i0 = np.repeat(np.arange(kernel_size), kernel_size)
-    i1 = np.repeat(np.arange(out_height), out_width)
-
-    i = i0.reshape((-1, 1)) + i1.reshape((1, -1))
-
-    j0 = np.tile(np.arange(kernel_size), kernel_size)
-    j1 = np.tile(np.arange(out_width), out_height)
-
-    j = j0.reshape((-1, 1)) + j1.reshape((1, -1))
-
-    img_cols = mat[i, j]
-    kernel_col = kernel.reshape((1, -1))
-
-    return np.matmul(kernel_col, img_cols).reshape((out_height, out_width))
+# def convolve(mat, kernel):
+#     return np.convolv(mat, kernel)
+# if FFT_CONVOLVE:
+#     return signal.fftconvolve(mat, kernel)
+# kernel_size = kernel.shape[0]
+#
+# out_height = mat.shape[0] - kernel_size + 1
+# out_width = mat.shape[1] - kernel_size + 1
+#
+# i0 = np.repeat(np.arange(kernel_size), kernel_size)
+# i1 = np.repeat(np.arange(out_height), out_width)
+#
+# i = i0.reshape((-1, 1)) + i1.reshape((1, -1))
+#
+# # j0 = np.tile(np.arange(kernel_size), kernel_size)
+# # j1 = np.tile(np.arange(out_width), out_height)
+# # j0 = np.array([*np.arange(kernel_size)] * kernel_size)
+# # j1 = np.array([*np.arange(out_width)] * out_height)
+# j0 = np.repeat(np.arange(kernel_size), kernel_size).reshape((-1, kernel_size)).transpose().flatten()
+# j1 = np.repeat(np.arange(out_width), out_height).reshape((-1, out_height)).transpose().flatten()
+#
+# j = j0.reshape((-1, 1)) + j1.reshape((1, -1))
+#
+# img_cols = mat[i, j]
+# kernel_col = kernel.reshape((1, -1))
+#
+# return np.matmul(kernel_col, img_cols).reshape((out_height, out_width))
 
 
 def naive_convolve(mat, kernel):
@@ -51,6 +72,7 @@ def naive_convolve(mat, kernel):
     return convolved_mat
 
 
+@jit(nopython=True)
 def Gaussian(mat, kernel_size=5, sigma=1.0):
     assert kernel_size % 2 == 1 and kernel_size > 0
     k = (kernel_size - 1) / 2
@@ -68,12 +90,23 @@ def Gaussian(mat, kernel_size=5, sigma=1.0):
     return convolve(mat, gaussian_filter)
 
 
-def Sobel(mat):
-    x_kernel = np.array([[1, 0, -1], [2, 0, -2], [1, 0, -1]], dtype=float)
-    y_kernel = np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]], dtype=float)
+x_kernel = np.array([[1, 0, -1], [2, 0, -2], [1, 0, -1]], dtype=float)
+y_kernel = np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]], dtype=float)
 
+
+@jit(float64[:, :](float64[:, :], int64, float64[:, :]), nopython=True)
+def rnd1(x, decimals, out):
+    return np.round_(x, decimals, out)
+
+
+@jit(nopython=True)
+def Sobel(mat):
     convolved_x = convolve(mat, x_kernel)
-    convolved_x[convolved_x == 0] = 1e-9
+    # convolved_x[convolved_x == 0] = 1e-9
+    for i in range(convolved_x.shape[0]):
+        for j in range(convolved_x.shape[1]):
+            if convolved_x[i, j] == 0:
+                convolved_x[i, j] = 1e-9
     convolved_y = convolve(mat, y_kernel)
 
     return np.sqrt(convolved_x * convolved_x + convolved_y * convolved_y), np.arctan(convolved_y / convolved_x)
@@ -103,13 +136,62 @@ def naive_nms(gradient, direction):
         return gradient
 
 
+@jit(nopython=True, cache=True)
+def CannyJIT(mat):
+    blurred = Gaussian(mat, kernel_size=5, sigma=1.4)
+    gradient, direction = Sobel(blurred)
+
+    direction *= 180 / np.pi
+
+    for y in range(1, gradient.shape[0] - 1):  # Technically not reaching edges, need to fix later
+        for x in range(1, gradient.shape[1] - 1):
+            val = gradient[y, x]
+            dir = direction[y, x]
+            if (dir < -67.5 or dir > 67.5) and (val <= gradient[y + 1, x] or val <= gradient[y - 1, x]):
+                gradient[y, x] = 0
+            elif dir < -22.5 and (val <= gradient[y + 1, x + 1] or val <= gradient[y - 1, x - 1]):
+                gradient[y, x] = 0
+            elif dir < 22.5 and (val <= gradient[y, x + 1] or val <= gradient[y, x - 1]):
+                gradient[y, x] = 0
+            elif val <= gradient[y + 1, x - 1] or val <= gradient[y - 1, x + 1]:
+                gradient[y, x] = 0
+
+    edge_strength = np.zeros(gradient.shape, dtype=np.uint8)
+    q = []
+    for i in range(edge_strength.shape[0]):
+        for j in range(edge_strength.shape[1]):
+            if gradient[i, j] > 50:
+                edge_strength[i, j] = 255
+                q.append([i, j])
+            elif gradient[i, j] > 10:
+                edge_strength[i, j] = 1
+
+    # Hysteresis
+
+    directions = [[-1, 1], [0, 1], [1, 1], [-1, 0], [1, 0], [-1, -1], [0, -1], [1, -1]]
+    while len(q):
+        ele = q.pop()
+        for dir in directions:
+            ny = ele[0] + dir[0]
+            nx = ele[1] + dir[1]
+            if 0 <= ny < edge_strength.shape[0] and 0 <= nx < edge_strength.shape[1] and \
+                    edge_strength[ny, nx] == 1:
+                edge_strength[ny, nx] = 255
+                q.append([ny, nx])
+
+    return edge_strength
+
+
+# @jit(nopython=True)
 def Canny(mat):
     blurred = Gaussian(mat, kernel_size=5, sigma=1.4)
     gradient, direction = Sobel(blurred)
 
     # Gradient Magnitude Thresholding
 
-    direction = np.round(direction / np.pi * 4) + 2
+    direction = np.zeros(direction.shape)
+    rnd1(direction / np.pi * 4, 0, direction)
+    direction += 2
 
     g0a = np.roll(gradient, (0, 1), axis=(0, 1))
     g0b = np.roll(gradient, (0, -1), axis=(0, 1))
@@ -190,15 +272,74 @@ def Canny(mat):
     return final
 
 
+# @jit(nopython=True)
+# def HoughLines(edge_mat):
+#     thetas = np.linspace(0, np.pi, 360)[:-1]
+#     edge_points = []
+#
+#     for y in range(edge_mat.shape[0]):
+#         for x in range(edge_mat.shape[1]):
+#             if edge_mat[y, x] == 255:
+#                 edge_points.append([y, x])
+#
+#     edge_points = np.array(edge_points)
+#     # edge_points = np.array(np.where(edge_mat == 255)).transpose()
+#
+#     rhos = np.matmul(edge_points, np.array([np.sin(thetas), np.cos(thetas)])).flatten()
+#
+#     hist = np.histogram2d(rhos, np.tile(thetas, edge_points.shape[0]), bins=(5000, thetas.size))
+#
+#     return hist
+
+
 def HoughLines(edge_mat):
     edge_points = np.array(np.where(edge_mat == 255)).transpose()
 
     thetas = np.linspace(0, np.pi, 360)[:-1]
     rhos = np.matmul(edge_points, np.array([np.sin(thetas), np.cos(thetas)])).flatten()
 
-    hist = np.histogram2d(rhos, np.tile(thetas, edge_points.shape[0]), bins=(5000, thetas.size))
+    hist = (
+        histogram2d(rhos, np.tile(thetas, edge_points.shape[0]),
+                    range=((np.min(rhos), np.max(rhos)), (0, np.max(thetas))),
+                    bins=(5000, thetas.size)),
+        np.linspace(np.min(rhos), np.max(rhos), 5000),
+        thetas)
 
-    return hist, maximum_filter(hist[0], size=9)
+    return hist
+
+
+# @jit(nopython=True)
+# def houghLines(edges, dTheta, threshold):
+#     imageShape = edges.shape
+#     imageDiameter = (imageShape[0] ** 2 + imageShape[1] ** 2) ** 0.5
+#     rhoRange = [i for i in range(int(imageDiameter) + 1)]
+#     thetaRange = [dTheta * i for i in range(int(-np.pi / (2 * dTheta)), int(np.pi / dTheta))]
+#     cosTheta = []
+#     sinTheta = []
+#     for theta in thetaRange:
+#         cosTheta.append(np.cos(theta))
+#         sinTheta.append(np.sin(theta))
+#     countMatrixSize = (len(rhoRange), len(thetaRange))
+#     countMatrix = np.zeros(countMatrixSize)
+#
+#     eds = []
+#     for (x, y), value in np.ndenumerate(edges):
+#         if value > 0:
+#             eds.append((x, y))
+#
+#     for thetaIndex in range(len(thetaRange)):
+#         theta = thetaRange[thetaIndex]
+#         cos = cosTheta[thetaIndex]
+#         sin = sinTheta[thetaIndex]
+#         for x, y in eds:
+#             targetRho = x * cos + y * sin
+#             closestRhoIndex = int(round(targetRho))
+#             countMatrix[closestRhoIndex, thetaIndex] += 1
+#     lines = []
+#     for (p, t), value in np.ndenumerate(countMatrix):
+#         if value > threshold:
+#             lines.append((p, thetaRange[t]))
+#     return lines
 
 
 if __name__ == '__main__':
@@ -253,26 +394,28 @@ if __name__ == '__main__':
     cv2.imshow("grayscale", grayscale)
 
     hough = HoughLines(canny)
+    max_filter = maximum_filter(hough[0], size=7)
+    # hough = houghLines(canny, 0.01, 20)
 
-    cv2.imshow("hist", hough[0][0] / np.max(hough[0][0]))
+    cv2.imshow("hist", hough[0] / np.max(hough[0]))
 
-    mat = np.repeat(hough[0][0] / np.max(hough[0][0]), 3).reshape((*hough[0][0].shape, 3))
-    mat[(hough[1] == hough[0][0]) & (hough[1] >= 50), 0] = 0
-    mat[(hough[1] == hough[0][0]) & (hough[1] >= 50), 1] = 0
-    mat[(hough[1] == hough[0][0]) & (hough[1] >= 50), 2] = 255
+    mat = np.repeat(hough[0] / np.max(hough[0]), 3).reshape((*hough[0].shape, 3))
+    mat[(max_filter == hough[0]) & (max_filter >= 50), 0] = 0
+    mat[(max_filter == hough[0]) & (max_filter >= 50), 1] = 0
+    mat[(max_filter == hough[0]) & (max_filter >= 50), 2] = 255
 
     mat = cv2.resize(mat, (1000, 1000))
 
     cv2.imshow("hist2", mat)
 
-    lines = np.array(np.where((hough[1] == hough[0][0]) & (hough[1] >= 50))).transpose()
+    lines = np.array(np.where((max_filter == hough[0]) & (max_filter >= 50))).transpose()
 
     canny = cv2.cvtColor(canny.astype(np.uint8), cv2.COLOR_GRAY2BGR)
 
     for line in lines:
         rho, theta = line
-        rho = hough[0][1][rho]
-        theta = hough[0][2][theta]
+        rho = hough[1][rho]
+        theta = hough[2][theta]
         a = np.cos(theta)
         b = np.sin(theta)
         x0 = a * rho
